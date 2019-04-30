13177d307336f33f031dc9289c6860254a29cc24
Adam Ladell <adamladell@outlook.com>
2019-04-30 15:47:58 +1000

    added custom fields to User model as well as added devise controllers and various test files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :enablers, dependent: :destroy
+  has_many :enablers, dependent: :destroy
   has_many :projects, dependent: :destroy
 end
