24affe837924cd5c16ebca111c4686d77dd102e0
Adam Ladell <adamladell@outlook.com>
2019-05-01 09:54:16 +1000

    added fundraiser user creation test

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,26 +1,34 @@
 require 'test_helper'
 
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user_params = users(:one)
+    @user = User.new(
+      email: Faker::Internet.email,
+      password: @user_params.encrypted_password,
+      password_confirmation: @user_params.encrypted_password,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      fundraiser: @user_params.fundraiser,
+      country: @user_params.country,
+      city: @user_params.city
+    )
+  end
+
   test 'should get sign up page' do
     get new_user_registration_path
 
     assert_response :success
   end
 
-  test 'should create fundraiser new user' do
-    @user = users(:one)
-    user = User.new(
-      email: Faker::Internet.email,
-      password: @user.encrypted_password,
-      password_confirmation: @user.encrypted_password,
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      fundraiser: @user.fundraiser,
-      country: @user.country,
-      city: @user.city
-    )
+  test 'should create new fundraiser user' do
+    @user.fundraiser = true
 
-    assert(user.save, user.errors.each { |k, v| puts "#{k}: #{v}" })
+    @user.save
+
+    user = User.find(@user.id)
+
+    assert_equal true, user.fundraiser, "User's fundraiser status is #{user.fundraiser}. Should be true!"
   end
 
   test 'should delete user' do
