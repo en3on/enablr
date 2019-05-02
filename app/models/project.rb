ca94acba9f82ac57de829008d96e8ee9e7bb2674
Adam Ladell <adamladell@outlook.com>
2019-05-02 13:01:48 +1000

    Started create project form and added categories method to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -21,6 +21,59 @@ class Project < ApplicationRecord
 
   before_create :set_initial_values
 
+
+  @software_categories = [
+                          'Mobile App',
+                          'Mobile Game',
+                          'Antivirus and Security',
+                          'Data Storage',
+                          'Development',
+                          'Web App',
+                          'Console Game',
+                          'Computer Game',
+                          'Operating System',
+                          'Office and Business',
+                          'Image, Video and Audio',
+                          'Personal Finance, Tax and Legal',
+                          'Education',
+                          'Web and Desktop Publishing',
+                          'Virtual Reality',
+                          'Other'
+                         ]
+
+   @hardware_categories = [
+                          'Cameras & Photo',
+                          'Cell Phones',
+                          'Smart Watches',
+                          'Accessories',
+                          'Computers',
+                          'Tablets',
+                          'Network Hardware',
+                          'TV, Video and Home Audio',
+                          'Portable Audio and Headphones',
+                          'Car Electronics',
+                          'Home Surveillance',
+                          'Smart Home',
+                          'Video and PC Gaming',
+                          'Virtual Reality',
+                          'Other'
+                        ]
+
+  def self.categories
+    categories = {}
+
+    @software_categories.each do |category|
+      categories[category] = 'software'
+    end
+
+    @hardware_categories.each do |category|
+      categories[category] = 'hardware'
+    end
+
+    categories
+  end
+
+
   private
   def set_initial_values
     self.current_amount = 0
