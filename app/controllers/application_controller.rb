87ced23975b3a03ca5646a0ba7a1e6e751045889
rommelryanrico <rommelryanrico@hotmail.com>
2019-05-03 16:50:05 +1000

    added forms to profile edit page from new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :fundraiser, :country, :city])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :fundraiser, :country, :city, :email, :password, :current_password])
   end
 end
