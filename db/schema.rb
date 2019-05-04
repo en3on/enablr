dd1ee16a45a97f9b6f42015c4fcc6a7e0c01897a
Adam Ladell <adamladell@outlook.com>
2019-05-04 20:00:13 +1000

    added name and rewards to perks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_094532) do
+ActiveRecord::Schema.define(version: 2019_05_04_095829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_05_04_094532) do
     t.datetime "updated_at", null: false
     t.bigint "project_id"
     t.integer "amount_left"
+    t.string "name"
+    t.string "rewards"
     t.index ["project_id"], name: "index_perks_on_project_id"
   end
 
