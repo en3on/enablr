cc03ee17b12e79805420a1e3d7ddb5538e0e9a35
Adam Ladell <adamladell@outlook.com>
2019-05-04 20:33:12 +1000

    added create method for perks

--- app/controllers/perks_controller.rb
+++ app/controllers/perks_controller.rb
@@ -4,4 +4,22 @@ class PerksController < ApplicationController
     @perk = Perk.new
     @project = Project.find(params[:project_id])
   end
+
+  def create
+    project = Project.find(params[:project_id])
+
+    perk = project.perks.new(perk_params(params[:perk]))
+    puts "Name: #{perk.name}"
+
+    if perk.save
+      redirect_to project
+    else
+      perk.errors.each { |n, e| puts "#{n}: #{e}" }
+    end
+  end
+
+  private
+  def perk_params(params)
+    params.permit(:name, :rewards, :minimum_amount, :amount_left)
+  end
 end
