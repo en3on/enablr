7d70659b05cb307d78b96008c8c79fbf12dfc2c3
Adam Ladell <adamladell@outlook.com>
2019-05-06 14:28:56 +1000

    Disallowed deleting perks with an enablr amount greater than 0

--- spec/models/perk_spec.rb
+++ spec/models/perk_spec.rb
@@ -1,24 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Perk, type: :model do
-  context 'with valid parameters' do
+  describe 'as an unlimited perk' do
+
     before(:each) do
       @perk = build(:unlimited_perk)
     end
 
-    it 'should be valid' do
-      expect(@perk).to be_valid
-    end
+    context 'with valid parameters' do
 
-    it 'should be unlimited' do
-      @perk.save
-      expect(@perk.unlimited).to be true
-    end
+      it 'should be valid' do
+        expect(@perk).to be_valid
+      end
 
-    it 'should be limited with an amount_left greater than -1' do
-      @perk.amount_left = 0
-      @perk.save
-      expect(@perk.unlimited).to be false
+      it 'should be unlimited' do
+        @perk.save
+        expect(@perk.unlimited).to be true
+      end
+
+      it 'should be limited with an amount_left greater than -1' do
+        @perk.amount_left = 0
+        @perk.save
+        expect(@perk.unlimited).to be false
+      end
     end
   end
 end
