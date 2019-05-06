5b7c466225635a46ad5d87cb39db2bb041052e79
Adam Ladell <adamladell@outlook.com>
2019-05-06 15:02:00 +1000

    Added avatar field to edit page and aws

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ private
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :fundraiser, :country, :city])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :fundraiser, :country, :city, :avatar])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :fundraiser, :country, :city, :email, :password, :current_password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :fundraiser, :country, :city, :email, :password, :current_password, :avatar])
   end
 end
