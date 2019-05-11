4d4b8d04be821a020ee3dec75d29789cfe498dd6
Adam Ladell <adamladell@outlook.com>
2019-05-11 16:04:23 +1000

    Added more data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,50 @@ puts 'Start of seeding'
 puts 'Creating projects with perks...'
 
 10.times.each do |i|
-  project = FactoryBot.build(:random)
-  project.name = @names[i]
-  project.save
+  @project = FactoryBot.build(:random)
+  @project.name = @names[i]
+  @project.save
 
   rand(1..5).times do
     num = rand(1..18)
-    project.picture.attach(io: File.open("app/assets/images/test-images/#{num}.jpg"), filename: "#{num}.jpg")
+    @project.picture.attach(io: File.open("app/assets/images/test-images/#{num}.jpg"), filename: "#{num}.jpg")
 
     puts "Attaching picture #{num}"
   end
 
-  puts "Creating project: #{project.name}"
+  puts "Creating project: #{@project.name}"
 
   rand(1..10).times do
     perk = FactoryBot.build(:random_perk_without_project)
-    perk.project_id = project.id
+    perk.project_id = @project.id
 
     puts "  Creating perk: #{perk.name}"
 
     perk.save
+  end
+
+  rand(1..10).times do
+    user = FactoryBot.create(:standard)
+    perk = @project.perks.sample
+
+    enablr = user.project_enablrs.new
+    enablr.pledged_amount = perk.minimum_amount
+    enablr.project_id = @project.id
+    enablr.perk_id = perk.id
+
+    @project.increment :current_amount, enablr.pledged_amount
+    @project.increment :backer_amount, 1
+    perk.increment :enablr_amount, 1
+
+    enablr.save
+    @project.save
+    perk.save
+
+    comment = enablr.comments.new
+    comment.content = Faker::Quote.yoda
+    comment.project_id = @project.id
+
+    comment.save
   end
 end
 
